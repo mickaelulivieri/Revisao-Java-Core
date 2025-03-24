2eb34fa8bae2838233aec113dac306925f18947f
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-24 17:42:51 -0300

    arraylist

---
+++ src/listas/ListaArray.java
@@ -0,0 +1,20 @@
+package listas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaArray {
+    public static void main(String[] args) {
+        /*
+        * Arraylist é uma subclasse de list
+        * */
+        List<String> lista = new ArrayList<>();
+
+        lista.add("Naruto Uzumaki");
+
+        lista.add("Sasuke");
+
+        System.out.println(lista);
+
+    }
+}
