c116eed3e779b1860a8717232048d4fc6f601b87
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-24 16:14:44 -0300

    arrays

---
+++ src/listas/Arrays.java
@@ -0,0 +1,21 @@
+package listas;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        String[] ninjas = new String[3];
+        int[] idade = new int[3];
+
+        ninjas[0] = "naruto";
+        ninjas[1] = "Sasuke";
+        ninjas[2] = "Sakura";
+
+        idade[0] = 16;
+        idade[1] = 19;
+        idade[2] = 14;
+
+        System.out.println(ninjas[1]);
+        System.out.println(idade[0]);
+
+    }
+}
